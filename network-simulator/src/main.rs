61fc2c4a951c5aeb73bd92895d4df5c063ba5c2f
Stewart Mackenzie <sjm@fractalide.com>
2019-11-01 03:05:31 +0800

    Problem: regression test of a simple fetch is insufficient
    Solution: create a small world graph, and, predictably, a problem
    was found. Will need to strip out the single thread and move
    back to process-per-router configuring the log output file.

--- network-simulator/src/main.rs
+++ network-simulator/src/main.rs
@@ -51,10 +51,10 @@ impl FromStr for Data {
 
 }
 
-fn router(faces: Vec<Face>, data: Option<Vec<Data>>, rx: Receiver<RouterControl>) -> JoinHandle<()> {
+fn router(faces: Vec<Face>, data: Option<Vec<Data>>, ctl_recv: Receiver<RouterControl>) -> JoinHandle<()> {
     let router = spawn( move || {
         let mut executor = ThreadPool::new().expect("Failed to create threadpool");
-        let mut r = Router::new(executor.clone(), rx);
+        let mut r = Router::new(executor.clone(), ctl_recv);
         for face in faces {
             let f = Udp::new(face.listen, face.remote);
             r.add_face(f);
@@ -73,20 +73,80 @@ fn router(faces: Vec<Face>, data: Option<Vec<Data>>, rx: Receiver<RouterControl>
 }
 
 fn simple_fetch() -> packets::Packet {
-    let (tx, rx) = unbounded();
+    let (ctl_send, ctl_recv) = unbounded();
     let node0 = vec![Face::from_str("127.0.0.1:8070|127.0.0.1:8071").unwrap(), Face::from_str("127.0.0.1:8072|127.0.0.1:8073").unwrap()];
-    router(node0, None, rx.clone());
+    router(node0, None, ctl_recv.clone());
     let node1 = vec![Face::from_str("127.0.0.1:8073|127.0.0.1:8072").unwrap(), Face::from_str("127.0.0.1:8074|127.0.0.1:8075").unwrap()];
-    router(node1, None, rx.clone());
+    router(node1, None, ctl_recv.clone());
     let node2 = vec![Face::from_str("127.0.0.1:8075|127.0.0.1:8074").unwrap(), Face::from_str("127.0.0.1:8076|127.0.0.1:8077").unwrap()];
-    router(node2, None, rx.clone());
+    router(node2, None, ctl_recv.clone());
     let node3_f = vec![Face::from_str("127.0.0.1:8077|127.0.0.1:8076").unwrap()];
     let node3_d = vec![Data::from_str("hello|world").unwrap()];
-    router(node3_f, Some(node3_d), rx.clone());
-    sleep(time::Duration::from_millis(1));
+    router(node3_f, Some(node3_d), ctl_recv.clone());
+    sleep(time::Duration::from_millis(10));
     let requestor = CopernicaRequestor::new("127.0.0.1:8071".into(), "127.0.0.1:8070".into());
     let response = requestor.request("hello".into());
-    tx.send(RouterControl::Exit).unwrap();
+    ctl_send.send(RouterControl::Exit).unwrap();
+    response
+}
+
+fn small_small_world_graph() -> packets::Packet {
+    // https://en.wikipedia.org/wiki/File:Small-world-network-example.png
+    let (ctl_send, ctl_recv) = unbounded(); // node 0 is the top node in the diagram, node 1 is clockwise one in the diagram
+    let node0 = vec![Face::from_str("127.0.0.1:50000|127.0.0.1:50001").unwrap(),    Face::from_str("127.0.0.1:50002|127.0.0.1:50003").unwrap(),
+                     Face::from_str("127.0.0.1:50029|127.0.0.1:50030").unwrap(),
+                     Face::from_str("127.0.0.1:50033|127.0.0.1:50034").unwrap(),
+                     Face::from_str("127.0.0.1:50037|127.0.0.1:50038").unwrap(),
+                     Face::from_str("127.0.0.1:50039|127.0.0.1:50040").unwrap(),
+                     Face::from_str("127.0.0.1:50046|127.0.0.1:50045").unwrap(),
+                     Face::from_str("127.0.0.1:50050|127.0.0.1:50049").unwrap()];
+    router(node0, None, ctl_recv.clone());
+    let node1 = vec![Face::from_str("127.0.0.1:50003|127.0.0.1:50002").unwrap(),    Face::from_str("127.0.0.1:50004|127.0.0.1:50005").unwrap()];
+    router(node1, None, ctl_recv.clone());
+    let node2 = vec![Face::from_str("127.0.0.1:50005|127.0.0.1:50004").unwrap(),    Face::from_str("127.0.0.1:50006|127.0.0.1:50007").unwrap(),
+                     Face::from_str("127.0.0.1:50030|127.0.0.1:50029").unwrap(),
+                     Face::from_str("127.0.0.1:50031|127.0.0.1:50032").unwrap()];
+    router(node2, None, ctl_recv.clone());
+    let node3 = vec![Face::from_str("127.0.0.1:50007|127.0.0.1:50006").unwrap(),    Face::from_str("127.0.0.1:50008|127.0.0.1:50009").unwrap(),
+                     Face::from_str("127.0.0.1:50034|127.0.0.1:50033").unwrap(),
+                     Face::from_str("127.0.0.1:50035|127.0.0.1:50036").unwrap()];
+    router(node3, None, ctl_recv.clone());
+    let node4 = vec![Face::from_str("127.0.0.1:50009|127.0.0.1:50008").unwrap(),    Face::from_str("127.0.0.1:50010|127.0.0.1:50011").unwrap(),
+                     Face::from_str("127.0.0.1:50032|127.0.0.1:50031").unwrap()];
+    router(node4, None, ctl_recv.clone());
+    let node5 = vec![Face::from_str("127.0.0.1:50011|127.0.0.1:50010").unwrap(),    Face::from_str("127.0.0.1:50012|127.0.0.1:50013").unwrap(),
+                     Face::from_str("127.0.0.1:50038|127.0.0.1:50037").unwrap()];
+    router(node5, None, ctl_recv.clone());
+    let node6 = vec![Face::from_str("127.0.0.1:50013|127.0.0.1:50012").unwrap(),    Face::from_str("127.0.0.1:50014|127.0.0.1:50015").unwrap(),
+                     Face::from_str("127.0.0.1:50027|127.0.0.1:50028").unwrap(),
+                     Face::from_str("127.0.0.1:50041|127.0.0.1:50042").unwrap()];
+    router(node6, None, ctl_recv.clone());
+    let node7 = vec![Face::from_str("127.0.0.1:50015|127.0.0.1:50014").unwrap(),    Face::from_str("127.0.0.1:50016|127.0.0.1:50017").unwrap(),
+                     Face::from_str("127.0.0.1:50036|127.0.0.1:50035").unwrap(),
+                     Face::from_str("127.0.0.1:50040|127.0.0.1:50039").unwrap(),
+                     Face::from_str("127.0.0.1:50043|127.0.0.1:50044").unwrap(),
+                     Face::from_str("127.0.0.1:50047|127.0.0.1:50048").unwrap()];
+    router(node7, None, ctl_recv.clone());
+    let node8 = vec![Face::from_str("127.0.0.1:50017|127.0.0.1:50016").unwrap(),    Face::from_str("127.0.0.1:50018|127.0.0.1:50019").unwrap(),
+                     Face::from_str("127.0.0.1:50042|127.0.0.1:50041").unwrap()];
+    router(node8, None, ctl_recv.clone());
+    let node9 = vec![Face::from_str("127.0.0.1:50019|127.0.0.1:50018").unwrap(),    Face::from_str("127.0.0.1:50020|127.0.0.1:50021").unwrap(),
+                     Face::from_str("127.0.0.1:50044|127.0.0.1:50043").unwrap(),
+                     Face::from_str("127.0.0.1:50045|127.0.0.1:50046").unwrap()];
+    router(node9, None, ctl_recv.clone());
+    let node10 = vec![Face::from_str("127.0.0.1:50021|127.0.0.1:50020").unwrap(),   Face::from_str("127.0.0.1:50022|127.0.0.1:50023").unwrap(),
+                      Face::from_str("127.0.0.1:50048|127.0.0.1:50047").unwrap(),
+                      Face::from_str("127.0.0.1:50049|127.0.0.1:50050").unwrap()];
+    router(node10, None, ctl_recv.clone());
+    let node11 = vec![Face::from_str("127.0.0.1:50023|127.0.0.1:50022").unwrap(),   Face::from_str("127.0.0.1:50024|127.0.0.1:50025").unwrap()];
+    router(node11, None, ctl_recv.clone());
+    let node12_f = vec![Face::from_str("127.0.0.1:50025|127.0.0.1:50026").unwrap(), Face::from_str("127.0.0.1:50001|127.0.0.1:50000").unwrap()];
+    let node12_d = vec![Data::from_str("hello|world").unwrap()];
+    router(node12_f, Some(node12_d), ctl_recv.clone());
+    sleep(time::Duration::from_millis(10));
+    let requestor = CopernicaRequestor::new("127.0.0.1:50028".into(), "127.0.0.1:50027".into());
+    let response = requestor.request("hello".into());
+    ctl_send.send(RouterControl::Exit).unwrap();
     response
 }
 
@@ -156,13 +216,20 @@ fn setup_logging(verbosity: u64, logpath: Option<&str>) -> Result<(), fern::Init
 }
 
 #[cfg(test)]
-mod network_regression_tests {
+mod network_regressions {
     use super::*;
 
     #[test]
     fn a_humble_four_hop_hello_world_fetch() {
         //setup_logging(3, None).unwrap();
         let packet = simple_fetch();
+        assert_eq!(response("hello".to_string(), "world".to_string().as_bytes().to_vec()), packet);
+    }
+
+    #[test]
+    fn a_small_small_world_graph() {
+        setup_logging(3, None).unwrap();
+        let packet = small_small_world_graph();
         assert_eq!(response("hello".to_string(), "world".to_string().as_bytes().to_vec()), packet);
     }
 }
