e39f94bc0b0c581009e0fa6311ffdf108742add8
Stewart Mackenzie <sjm@fractalide.com>
2019-11-23 06:46:03 +0800

    Problem: Packets larger than UDP's MTU needs to be broken up
    Solution: break up a file into 1024 byte chunks.
    The first response contains how many chunks the total batch
    consists of. The first packet uses the same name as the request
    name.
    i.e.
    
    name: PiratesoftheCaribbean content: PiratesOfTheCaribbean-10000
    the name converts to vec![vec![806, 1462, 1766]]
    
    The second packet will be named
    PiratesOfTheCarribean/0
    which'll translate to an sdri
    vec![vec![806, 1462, 1766], vec![2, 482, 1250]]
    
    The third packet will be named
    PiratesOfTheCarribean/1
    which translates to an sdri
    vec![vec![806, 1462, 1766], vec![71, 1415, 1687]]
    
    So this way the initial search for a packet will hit a
    face with the correct breadcrumb.
    Thereafter we can query for the union of the sequence and name
    
    Importantly, the sequence needs to have the <name>-sequence
    
    If we don't do this then every sequence packet will poison every face
    so sequences need uniqueness

--- packets/src/lib.rs
+++ packets/src/lib.rs
@@ -37,12 +37,54 @@ pub fn response(name: String, data: Data) -> Packet {
     }
 }
 
+pub fn mk_response(name: String, data: Data) -> Vec<(String, Packet)> {
+    let chunks = data.chunks(1024);
+    let mut out: Vec<(String, Packet)> = vec![];
+    let sequence: String = format!("{}\n{}", name.clone(), chunks.len() - 1);
+    //println!("sequence: {}", sequence.clone());
+    let mut count: usize = 0;
+    out.push((name.clone(), response(name.clone(), sequence.as_bytes().to_vec())));
+    for chunk in chunks {
+        let chunk_name: String = format!("{}/{}", name.clone(), count);
+        //println!("chunk name: {}", chunk_name.clone());
+        out.push((chunk_name.clone(), response(chunk_name, chunk.to_vec())));
+        count += 1;
+    }
+    out
+}
+
 impl fmt::Debug for Packet {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &*self {
             Packet::Request{sdri} => write!(f, "REQ{:?}", sdri),
             Packet::Response{sdri, ..} => write!(f, "RES{:?}", sdri),
         }
+    }
+}
+
+#[cfg(test)]
+mod responses {
+    use super::*;
+
+    #[test]
+    fn make_chunks() {
+        let len = 1025;
+        let zero_vec = vec![0; len];
+        let responses = mk_response("ThePiratesOfTheCaribbean".to_string(), zero_vec);
+        assert_eq!( vec![
+            ("ThePiratesOfTheCaribbean".to_string(), Packet::Response {
+                sdri: vec![vec![806, 1462, 1766]],
+                data: "ThePiratesOfTheCaribbean\n1".as_bytes().to_vec(),
+            }),
+            ("ThePiratesOfTheCaribbean/0".to_string(), Packet::Response {
+                sdri: vec![vec![806, 1462, 1766], vec![2, 482, 1250]],
+                data: vec![0; 1024],
+            }),
+            ("ThePiratesOfTheCaribbean/1".to_string(), Packet::Response {
+                sdri: vec![vec![806, 1462, 1766], vec![71, 1415, 1687]],
+                data: vec![0; 1],
+            }),
+            ] , responses);
     }
 }
 
