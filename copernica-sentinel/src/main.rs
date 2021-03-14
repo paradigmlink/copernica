de10f2a024f05782b9246363aab2097c98cdfec7
Stewart Mackenzie <sjm@fractalide.com>
2021-03-14 21:03:56 +0800

    Problem: uncertain if the board is actually working
    Solution: make a blinker binary

--- copernica-sentinel/src/main.rs
+++ copernica-sentinel/src/main.rs
@@ -1,21 +1,44 @@
 #![feature(default_alloc_error_handler)]
 #![no_std]
 #![no_main]
-use cortex_m_rt::{entry, exception};
-use cortex_m::{asm, peripheral::SCB};
-use rtt_target::{rprintln, rtt_init_print};
+use {
+    cortex_m_rt::{entry, exception},
+    cortex_m::{asm},
+    rtt_target::{rprintln, rtt_init_print},
+    embedded_hal::{
+        digital::v2::OutputPin,
+    },
+    nb::block,
+    stm32f1xx_hal::{
+        prelude::*,
+        timer::Timer,
+    },
+};
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello world with a stacktrace!");
-    SCB::set_pendsv();
-    rprintln!("after PendSV");
-    exit()
-}
-#[exception]
-fn PendSV() {
-    rprintln!("PendSV");
-    panic!()
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f1xx_hal::pac::Peripherals::take().unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.mhz())
+        .sysclk(48.mhz())
+        .pclk1(24.mhz())
+        .freeze(&mut flash.acr);
+    assert!(clocks.usbclk_valid());
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut led = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
+    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(3.hz());
+    loop {
+        rprintln!("Off");
+        block!(timer.wait()).unwrap();
+        led.set_high().unwrap();
+        rprintln!("On");
+        block!(timer.wait()).unwrap();
+        led.set_low().unwrap();
+    }
 }
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
